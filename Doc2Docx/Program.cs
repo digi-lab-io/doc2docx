d10da8964f7077afafb62933f6ca7015dea211d3
4F2E4A2E <4F2E4A2E@users.noreply.github.com>
2015-10-01 13:27:16 +0200

    Adding initial commit

---
+++ Doc2Docx/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+using System.Reflection;
+using System.IO;
+
+namespace Doc2Docx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                FileInfo file = new FileInfo(@args[0]);
+
+                if (file.Extension.ToLower() == ".doc" || file.Extension.ToLower() == ".xml" || file.Extension.ToLower() == ".wml")
+                {
+                    Word._Application application = new Word.Application();
+                    object fileformat = Word.WdSaveFormat.wdFormatXMLDocument;
+
+                    object filename = file.FullName;
+                    object newfilename = Path.ChangeExtension(file.FullName, ".docx");
+                    Word._Document document = application.Documents.Open(filename);
+
+                    document.Convert();
+                    document.SaveAs(newfilename, fileformat);
+                    document.Close();
+                    document = null;
+
+                    application.Quit();
+                    application = null;
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Missing parameter: IndexOutOfRangeException {0}", e);
+            }
+
+        }
+    }
+}

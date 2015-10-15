21a7ab5dafbe5dd1b2c944a9c5049958605266bf
4F2E4A2E <4F2E4A2E@users.noreply.github.com>
2015-10-15 08:46:57 +0200

    * Implemented basic Error handling * Improved Winword process termination * Added logg messages

--- Doc2Docx/Program.cs
+++ Doc2Docx/Program.cs
@@ -23,6 +23,8 @@ using System.Threading.Tasks;
 using Word = Microsoft.Office.Interop.Word;
 using System.Reflection;
 using System.IO;
+using Microsoft.Office.Interop.Word;
+using System.Diagnostics;
 
 namespace Doc2Docx
 {
@@ -30,32 +32,62 @@ namespace Doc2Docx
     {
         static void Main(string[] args)
         {
+            FileInfo file = new FileInfo(@args[0]);
 
-            try
+            if (file.Extension.ToLower() == ".doc" || file.Extension.ToLower() == ".xml" || file.Extension.ToLower() == ".wml")
             {
-                FileInfo file = new FileInfo(@args[0]);
 
-                if (file.Extension.ToLower() == ".doc" || file.Extension.ToLower() == ".xml" || file.Extension.ToLower() == ".wml")
+                Console.WriteLine("Starting document conversion...");
+                // Get all running winword processes
+                List<int> processIds = new List<int>();
+                foreach (Process process in Process.GetProcessesByName("WINWORD"))
                 {
-                    Word._Application application = new Word.Application();
+                    processIds.Add(process.Id);
+                }
+
+                Word._Application word = new Word.Application();
+
+                try
+                {
+
                     object fileformat = Word.WdSaveFormat.wdFormatXMLDocument;
 
                     object filename = file.FullName;
                     object newfilename = Path.ChangeExtension(file.FullName, ".docx");
-                    Word._Document document = application.Documents.Open(filename);
+                    Word._Document document = word.Documents.Open(filename);
+                    Console.WriteLine("Converting document '{0}' to DOCX.", file);
 
                     document.Convert();
                     document.SaveAs(newfilename, fileformat);
                     document.Close();
                     document = null;
 
-                    application.Quit();
-                    application = null;
+                    word.Quit();
+                    word = null;
+                    Console.WriteLine("Success, quitting Word.");
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error ocurred: {0}", e);
+                }
+                finally
+                {
+
+                    // and here is how it fails
+                    foreach (Process process in Process.GetProcessesByName("WINWORD"))
+                    {
+                        if (!processIds.Contains(process.Id))
+                        {
+                            Console.WriteLine("Terminating Winword process with the ID: '{0}'.", process.Id);
+                            process.Kill();
+                        }
+                    }
                 }
             }
-            catch (IndexOutOfRangeException e)
+            else
             {
-                Console.WriteLine("Missing parameter: IndexOutOfRangeException {0}", e);
+                Console.WriteLine("Only DOC / WML / XML files possible.");
             }
 
         }
